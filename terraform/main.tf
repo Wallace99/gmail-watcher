c7badaea46f5336824f82323dd94b0b74ec1d0ba
Wallace Sharpe-Davidson <wallace722@gmail.com>
2023-12-23 08:01:53 +1300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,7 +88,7 @@ resource "google_storage_bucket_iam_member" "storage_member" {
 
 resource "google_cloud_scheduler_job" "trigger_job" {
   name             = "schedule-gmail-watcher"
-  schedule         = "0 8 * * *"
+  schedule         = "0 0 * * *"
   attempt_deadline = "320s"
   region           = var.location
   project          = var.project_id
